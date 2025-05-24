c8cdb18ef7669790d8dbc7ff93695c62c93a0a1c
TNTSuperMan <abworld@outlook.jp>
2025-05-24 14:38:40 +0900

    ログ出力方法とかをかえる

--- runtimes/optimizedjit.ts
+++ runtimes/optimizedjit.ts
@@ -1,28 +1,33 @@
 import { execRecord } from "../src";
 
 export const BFOptimizedWASMJIT = async (source: string) => {
+    const start = performance.now();
     const exr = await execRecord(source)
-
-    console.log(`parse: ${exr[1]}`)
-    console.log(`bundle: ${exr[2]}`)
-    console.log(`optimize: ${exr[3]}`)
-    console.log(`watgen: ${exr[4]}`)
-    console.log(`wasmgen: ${exr[5]}`)
 
     let stdout = "";
 
-    console.time("wasm init");
+    const sn = performance.now();
     const instance = await WebAssembly.instantiate(exr[0], {
         js: {
             stdout(t: number){ stdout += String.fromCharCode(t) },
             stdin(){ return 0 }
         }
     })
-    console.timeEnd("wasm init")
-    console.time("run")
-    //@ts-ignore
-    instance.instance.exports.run()
+    const en = performance.now();
 
-    console.timeEnd("run")
+    const rs = performance.now(); //@ts-ignore
+    instance.instance.exports.run()
+    const re = performance.now()
+
+    const end = performance.now();
+
+    console.log(`parse: ${exr[1]}`)
+    console.log(`bundle: ${exr[2]}`)
+    console.log(`optimize: ${exr[3]}`)
+    console.log(`watgen: ${exr[4]}`)
+    console.log(`wasmgen: ${exr[5]}`)
+    console.log(`wasmload: ${en-sn}`)
+    console.log(`run: ${re-rs}`)
+    console.log(`total: ${end-start}`)
     return stdout;
 }
